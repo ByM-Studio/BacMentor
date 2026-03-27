16352cf88dc60b81cb7bc7ed8e8650c14b4da045
ByM-Studio <mehdi.webdesign01@gmail.com>
2026-03-27 22:47:11 +0100

    Update auth.js

--- api/auth.js
+++ api/auth.js
@@ -119,7 +119,7 @@ export default async function handler(req, res) {
     passwordHash: user.passwordHash,
     createdAt: user.createdAt,
     premium: 'false',
-    premiumSince: ''
+    premiumSince: '0'
   });
   return res.status(200).json({ ok: true });
 }
